0faeea3f48ab836c1e3564cd36539130740f5912
ni.pizani <106701222+nipizani06@users.noreply.github.com>
2024-11-06 15:21:46 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 
 
 function setup() {
-  createCanvas(400, 400, WEBGL); //plano 3D
+  createCanvas(1920, 1080, WEBGL); //plano 3D
   terra=loadImage('terra.jpg')
 }
 
@@ -9,7 +9,7 @@ function draw() {
   background(1000, 1000, 1000); //cor de fundo
   rotateY(millis()/1000); // rotação da terra
   texture(terra); //textura da terra, imagem.
-  sphere(140, 160); //esfera 3D
+  sphere(200, 2200); //esfera 3D
   
   
 }
